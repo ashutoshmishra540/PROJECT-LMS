86537e556982c56e637793428ed55d6f17ec7c6a
ashutoshmishra540 <124282090+ashutoshmishra540@users.noreply.github.com>
2024-02-12 13:26:18 +0000

    lms

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 
+
+const connectToDb = require('./config/db.js')
+
 const app = express();
+
+// init connection to db
+connectToDb()
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
